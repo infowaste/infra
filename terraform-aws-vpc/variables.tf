363f7883df6700ac8d08e87335062b812b9c65f2
dexter <psi.dexter@gmail.com>
2019-02-25 21:46:37 +0300

    fix cidr 1

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -10,7 +10,7 @@ variable "name" {
 
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
-  default     = "10.0.0.0/16"
+  default     = "0.0.0.0/0"
 }
 
 variable "assign_generated_ipv6_cidr_block" {
